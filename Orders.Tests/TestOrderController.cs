14b9dea50af369851a5a067faf7a9ab2cfaacd79
Neetendra <singhsneetendra@gmail.com>
2019-10-24 22:41:56 +0200

    Added more test cases.

--- Orders.Tests/TestOrderController.cs
+++ Orders.Tests/TestOrderController.cs
@@ -8,7 +8,7 @@ namespace Orders.Tests
     {
 
         [Fact]
-        public void AddInvalidOrderList()
+        public void InvalidOrderList()
         {
             //Arrange 
             List<ObjectType> inValidOrderList = new List<ObjectType>()
@@ -23,16 +23,16 @@ namespace Orders.Tests
             };
 
             //Act
-            bool badResponse;
+            bool response;
             string msg;
-            (badResponse, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(inValidOrderList);
 
             // Assert
-            Assert.False(badResponse, msg);
+            Assert.False(response, msg);
         }
 
         [Fact]
-        public void AddValidOrderList()
+        public void ValidOrderList()
         {
             //Arrange 
             List<ObjectType> inValidOrderList = new List<ObjectType>()
@@ -47,12 +47,60 @@ namespace Orders.Tests
             };
 
             //Act
-            bool badResponse;
+            bool response;
             string msg;
-            (badResponse, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(inValidOrderList);
 
             // Assert
-            Assert.True(badResponse, msg);
+            Assert.True(response, msg);
+        }
+
+        [Fact]
+        public void InvalidObjectId()
+        {
+            //Arrange 
+            List<ObjectType> inValidOrderList = new List<ObjectType>()
+
+            {
+                new ObjectType {
+                ObjectID = 0,
+                ObjectName = "C# Book",
+                ObjectPrice = 3,
+                ObjectQuantity = "10 pieces"
+                }
+            };
+
+            //Act
+            bool response;
+            string msg;
+            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+
+            // Assert
+            Assert.False(response, msg);
+        }
+
+        [Fact]
+        public void ValidObjectId()
+        {
+            //Arrange 
+            List<ObjectType> inValidOrderList = new List<ObjectType>()
+
+            {
+                new ObjectType {
+                ObjectID = 1,
+                ObjectName = "C# Book",
+                ObjectPrice = 3,
+                ObjectQuantity = "10 pieces"
+                }
+            };
+
+            //Act
+            bool response;
+            string msg;
+            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+
+            // Assert
+            Assert.True(response, msg);
         }
     }
 }
