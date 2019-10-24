78b239bd9ac93282ea1e0b4ee036b477e47877f6
Neetendra <singhsneetendra@gmail.com>
2019-10-24 23:28:57 +0200

    Changed to correct naming convention.

--- Orders.Tests/TestOrder.cs
+++ Orders.Tests/TestOrder.cs
@@ -1,17 +1,18 @@
 using System.Collections.Generic;
 using Orders.Models;
 using Xunit;
+
 namespace Orders.Tests
 {
 
-    public class TestOrderController
+    public class TestOrder
     {
 
         [Fact]
         public void InvalidOrderList()
         {
             //Arrange 
-            List<ObjectType> inValidOrderList = new List<ObjectType>()
+            List<ObjectType> orderList = new List<ObjectType>()
 
             {
                 new ObjectType {
@@ -25,7 +26,7 @@ namespace Orders.Tests
             //Act
             bool response;
             string msg;
-            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(orderList);
 
             // Assert
             Assert.False(response, msg);
@@ -35,7 +36,7 @@ namespace Orders.Tests
         public void ValidOrderList()
         {
             //Arrange 
-            List<ObjectType> inValidOrderList = new List<ObjectType>()
+            List<ObjectType> orderList = new List<ObjectType>()
 
             {
                 new ObjectType {
@@ -49,7 +50,7 @@ namespace Orders.Tests
             //Act
             bool response;
             string msg;
-            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(orderList);
 
             // Assert
             Assert.True(response, msg);
@@ -59,7 +60,7 @@ namespace Orders.Tests
         public void InvalidObjectId()
         {
             //Arrange 
-            List<ObjectType> inValidOrderList = new List<ObjectType>()
+            List<ObjectType> orderList = new List<ObjectType>()
 
             {
                 new ObjectType {
@@ -73,7 +74,7 @@ namespace Orders.Tests
             //Act
             bool response;
             string msg;
-            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(orderList);
 
             // Assert
             Assert.False(response, msg);
@@ -83,7 +84,7 @@ namespace Orders.Tests
         public void ValidObjectId()
         {
             //Arrange 
-            List<ObjectType> inValidOrderList = new List<ObjectType>()
+            List<ObjectType> orderList = new List<ObjectType>()
 
             {
                 new ObjectType {
@@ -97,7 +98,55 @@ namespace Orders.Tests
             //Act
             bool response;
             string msg;
-            (response, msg) = Order.ValidateOrderList(inValidOrderList);
+            (response, msg) = Order.ValidateOrderList(orderList);
+
+            // Assert
+            Assert.True(response, msg);
+        }
+
+        [Fact]
+        public void InValidObjectPrice()
+        {
+            //Arrange 
+            List<ObjectType> orderList = new List<ObjectType>()
+
+            {
+                new ObjectType {
+                ObjectID = 1,
+                ObjectName = "C# Book",
+                ObjectPrice = 0,
+                ObjectQuantity = "10 pieces"
+                }
+            };
+
+            //Act
+            bool response;
+            string msg;
+            (response, msg) = Order.ValidateOrderList(orderList);
+
+            // Assert
+            Assert.False(response, msg);
+        }
+
+        [Fact]
+        public void ValidObjectPrice()
+        {
+            //Arrange 
+            List<ObjectType> orderList = new List<ObjectType>()
+
+            {
+                new ObjectType {
+                ObjectID = 1,
+                ObjectName = "C# Book",
+                ObjectPrice = 3,
+                ObjectQuantity = "10 pieces"
+                }
+            };
+
+            //Act
+            bool response;
+            string msg;
+            (response, msg) = Order.ValidateOrderList(orderList);
 
             // Assert
             Assert.True(response, msg);
